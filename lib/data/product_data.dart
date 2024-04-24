cab4d6207fcd80dd362ccbe4d3a8c01409c55534
IKER_SAMUEL_REQUENES_HERNANDEZ <a.21308051280397@cbtis128.edu.mx>
2024-04-24 15:48:24 +0000

    actualizado

---
+++ lib/data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:uii_a6_disenio/modelos/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/jordan.png",
+      title: "Jordan Tradicional",
+      description: "Mas vendidos"),
+  ProductModel(
+      img: "assets/images/jordan1.png",
+      title: "Jordan Blue&White",
+      description: "Los especiales de la casa"),
+  ProductModel(
+      img: "assets/images/jordan2.png",
+      title: "Air Jordan",
+      description: "Nuevos, pero especiales"),
+];
